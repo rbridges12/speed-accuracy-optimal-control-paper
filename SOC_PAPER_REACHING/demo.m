6a86d83c30de96dd028c6be8d6c3d764b266352f
Ethan <Ethan@DESKTOP-ETHAN>
2024-04-26 07:35:52 -0400

    "Fitts Law and Normalized Velocity Plotting"

--- SOC_PAPER_REACHING/demo.m
+++ SOC_PAPER_REACHING/demo.m
@@ -12,12 +12,12 @@ addpath("./plotFunctions")
 
 % set model and optimization parameters
 N = 80; % number of discretized nodes
-wM_std = 0.03; % motor noise standard deviation
+wM_std = 0.01; % motor noise standard deviation
 EE_init = [0; 0.3];
-EE_target = [0; 0.45];
+EE_target = [0; .45];
 final_pos_variance_95 = 0.03 ; % 95% confidence interval for final position radius
 final_vel_variance_95 = 0.1; % 95% confidence interval for final velocity radius
-k_u = 0.25; % control effort weight
+k_u = .05; % control effort weight
 k_t = 1; % duration weight
 
 result = optimization_6muscles(N, wM_std, final_pos_variance_95, final_vel_variance_95, k_u, k_t, EE_init, EE_target);
@@ -31,18 +31,23 @@ covs = result.P_EEPos(:, end);
 P_EE_final = [covs(1) covs(2); covs(2) covs(3)]
 target_P = [(final_pos_variance_95/2)^2 0; 0 (final_pos_variance_95/2)^2]
 
-figure
-hold on
-error_ellipse(P_EE_final, [0; 0], 0.95, "k:");
-error_ellipse(target_P, [0; 0], 0.95, "r:");
-legend("actual", "target")
-hold off
+% figure
+% hold on
+% error_ellipse(P_EE_final, [0; 0], 0.95, "k:");
+% error_ellipse(target_P, [0; 0], 0.95, "r:");
+% legend("actual", "target")
+% hold off
 
 %%
 animate_trajectory(result);
 
 %%
-plotNormalizedVelocity(result);
+% plotNormalizedVelocity(result);
 
 %%
-plotResults(result);
+% plotResults(result);
+
+%% 
+format long
+result.time(length(result.time))
+
--- SOC_PAPER_REACHING/plotFunctions/plotNormalizedVelocity.m
+++ SOC_PAPER_REACHING/plotFunctions/plotNormalizedVelocity.m
@@ -1,10 +1,12 @@
 function plotNormalizedVelocity(result)
     EE_vel = result.EEVel;
-    norm_vel = vecnorm(EE_vel,2,2)
+    norm_vel = vecnorm(EE_vel,2,2);
+    normalized_vel = norm_vel./max(norm_vel);
+    normalized_time = result.time./max(result.time);
 
     figure
-    plot(result.time, norm_vel, 'LineWidth', 2)
+    plot(normalized_time, normalized_vel, 'LineWidth', 2)
     title('Normalized Velocity of End Effector')
-    xlabel('Time (s)')
-    ylabel('Normalized Velocity (m/s)')
+    xlabel('Normalized Time')
+    ylabel('Normalized Velocity')
 end
