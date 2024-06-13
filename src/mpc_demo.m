903f675b42694ddac125049d127ece1bfb861b7a
Riley Bridges <rlybrdgs@gmail.com>
2024-06-13 10:18:16 -0400

    empty mpc script

---
+++ src/mpc_demo.m
@@ -0,0 +1,9 @@
+% include functions in subdirectories
+addpath("~/casadi-3.6.5")
+addpath("./forwardSim")
+addpath("./Muscle_LMT_dM")
+addpath("./MuscleModel")
+addpath("./ArmModel")
+addpath("./MusculoskeletalDynamics")
+addpath("./Integrator")
+addpath("./plotFunctions")
